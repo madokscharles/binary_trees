40eb53fe2fb7a7ab13694275d4aee5fbabacd7cd
madokscharles <charlesmadoks@gmail.com>
2023-03-02 20:34:33 +0100

    Fix error

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,7 +5,6 @@
  *
  * @parent: - is a pointer to the parent node of the node to create
  * @value: - is the value to put in the new node
-
  * Return: a pointer to the new node or NULL on failure
  */
 
